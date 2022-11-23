778e744267f7eb4f3d252e29ef7fa8bbafd6a84c
Iver Småge men bærbar <iversmage@gmail.com>
2022-11-23 14:27:42 +0100

    make innholdsfortegnelse readme

--- helpers/ihf.py
+++ helpers/ihf.py
@@ -5,6 +5,6 @@ from pathlib import Path
 def show_ihf():
     thisfolder = Path(__file__).parent
     # thisfolder = Path("./")
-    with open(thisfolder / "innholdsfortegnelse.md") as mdfile:
+    with open(thisfolder / ".." / "README.md") as mdfile:
         mdtxt = mdfile.read()
         display(Markdown(mdtxt))
